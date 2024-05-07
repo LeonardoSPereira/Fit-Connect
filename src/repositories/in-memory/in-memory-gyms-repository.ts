eb23d99f25fb30b6f3943db4709e1c936844201c
Leonardo Pereira <leonardo_human@hotmail.com>
2024-05-07 10:43:26 -0300

    feat: search gyms use-case & tests

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -5,6 +5,21 @@ import { randomUUID } from "node:crypto";
 export class InMemoryGymsRepository implements GymsRepository {
   public gyms: Gym[] = [];
 
+  async create(data: Prisma.GymCreateInput) {
+    const gym = {
+      id: data.id ?? randomUUID(),
+      title: data.title,
+      description: data.description ?? null,
+      phone: data.phone ?? null,
+      latitude: new Prisma.Decimal(data.latitude.toString()),
+      longitude: new Prisma.Decimal(data.longitude.toString()),
+    }
+
+    this.gyms.push(gym)
+
+    return gym
+  }
+
   async findById(id: string) {
     const gym = this.gyms.find(gym => gym.id === id)
 
@@ -15,19 +30,9 @@ export class InMemoryGymsRepository implements GymsRepository {
     return gym
   }
 
-  async create(data: Gym) {
-    const gym = {
-      id: data.id ?? randomUUID(),
-      title: data.title,
-      description: data.description,
-      phone: data.phone,
-      latitude: new Prisma.Decimal(data.latitude.toString()),
-      longitude: new Prisma.Decimal(data.longitude.toString()),
-    }
-
-    this.gyms.push(gym)
-
-    return gym
+  async searchMany(query: string, page: number) {
+    return this.gyms
+      .filter(gym => gym.title.includes(query))
+      .slice((page - 1) * 20, page * 20)
   }
-
 }
--- src/use-cases/search-gyms.spec.ts
+++ src/use-cases/search-gyms.spec.ts
@@ -1,30 +1,65 @@
 import { it, expect, describe, beforeEach } from 'vitest'
-import { RegisterUseCase } from './register'
-import { compare } from 'bcryptjs'
-import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
-import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
-import { CreateGymUseCase } from './create-gym'
+import { SearchGymsUseCase } from './search-gyms'
 import { Decimal } from '@prisma/client/runtime/library'
 
 let gymsRepository: InMemoryGymsRepository
-let sut: CreateGymUseCase
+let sut: SearchGymsUseCase
 
-describe("Create Gym Use Case", () => {
-  beforeEach(() => {
+describe("Search Gyms Use Case", () => {
+  beforeEach(async () => {
     gymsRepository = new InMemoryGymsRepository()
-    sut = new CreateGymUseCase(gymsRepository)
+    sut = new SearchGymsUseCase(gymsRepository)
   })
 
-  it("should be able to create gym", async () => {
-    const { gym } = await sut.execute({
-      title: "Academia",
+  it("should be able to search for gyms", async () => {
+    await gymsRepository.create({
+      title: "Javascript Academy",
       description: "Academia de musculação",
       phone: "123456",
-      latitude: 123456,
-      longitude: 123456,
+      latitude: new Decimal(123456),
+      longitude: new Decimal(123456),
     })
 
-    expect(gym.id).toEqual(expect.any(String))
+    await gymsRepository.create({
+      title: "Typescript Academy",
+      description: "Academia de musculação",
+      phone: "123456",
+      latitude: new Decimal(123456),
+      longitude: new Decimal(123456),
+    })
+
+    const { gyms } = await sut.execute({
+      query: "Javascript",
+      page: 1
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: "Javascript Academy"})
+    ])
+  })
+
+  it("should be able to fetch paginated gym search", async () => {
+    for(let i = 1; i <= 22; i++) {
+      await gymsRepository.create({
+        title: `Javascript Academy ${i}`,
+        description: "Academia de musculação",
+        phone: "123456",
+        latitude: new Decimal(123456),
+        longitude: new Decimal(123456),
+      })
+    }
+
+    const { gyms } = await sut.execute({
+      query: "Javascript",
+      page: 2
+    })
+
+    expect(gyms).toHaveLength(2)
+    expect(gyms).toEqual([
+      expect.objectContaining({ title: "Javascript Academy 21"}),
+      expect.objectContaining({ title: "Javascript Academy 22"})
+    ])
   })
 })
