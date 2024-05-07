7bcd6a20852abf01457c5d121f2fc368e24e6dc2
Leonardo Pereira <leonardo_human@hotmail.com>
2024-05-07 10:22:54 -0300

    feat: get user metrics use-case & tests

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -46,4 +46,8 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
     return checkIns
   }
+
+  async countByUserId(userId: string) {
+    return this.checkIns.filter(checkIn => checkIn.user_id === userId).length
+  }
 }
---
+++ src/use-cases/search-gyms.spec.ts
@@ -0,0 +1,30 @@
+import { it, expect, describe, beforeEach } from 'vitest'
+import { RegisterUseCase } from './register'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+import { InMemoryGymsRepository } from '@/repositories/in-memory/in-memory-gyms-repository'
+import { CreateGymUseCase } from './create-gym'
+import { Decimal } from '@prisma/client/runtime/library'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: CreateGymUseCase
+
+describe("Create Gym Use Case", () => {
+  beforeEach(() => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new CreateGymUseCase(gymsRepository)
+  })
+
+  it("should be able to create gym", async () => {
+    const { gym } = await sut.execute({
+      title: "Academia",
+      description: "Academia de musculação",
+      phone: "123456",
+      latitude: 123456,
+      longitude: 123456,
+    })
+
+    expect(gym.id).toEqual(expect.any(String))
+  })
+})
