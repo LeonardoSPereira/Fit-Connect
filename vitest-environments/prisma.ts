c779651bba090743d4c31b3e80df6fb69bf777dc
Leonardo Pereira <leonardo_human@hotmail.com>
2024-05-08 14:38:11 -0300

    feat: implemented e2e prisma environment configuration

--- vitest-environments/prisma.ts
+++ vitest-environments/prisma.ts
@@ -1,14 +1,28 @@
+import { generateDatabaseUrl } from '@/utils/generate-database-url';
+import { PrismaClient } from '@prisma/client';
+import { execSync } from 'node:child_process';
+import { randomUUID } from 'node:crypto';
 import type { Environment } from 'vitest'
 
+const prisma = new PrismaClient();
+
 export default <Environment>{
-  name: 'custom',
+  name: 'prisma',
   transformMode: 'ssr',
-  setup() {
-    console.log('setup');
-    
+  async setup() {
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseUrl(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    execSync('npx prisma migrate deploy')
+
     return {
-      teardown() {
-        console.log('teardown');
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        )
+        await prisma.$disconnect()
       }
     }
   }
